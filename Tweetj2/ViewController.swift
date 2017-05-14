0682bae1598fa0b46ea5f769eada886e73255892
Ari Munandar <arimunandar.dev@gmail.com>
2017-05-14 21:53:13 +0700

    Initial Commit

---
+++ Tweetj2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tweetj2
+//
+//  Created by Jerry on 5/14/17.
+//  Copyright © 2017 Next Project. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
